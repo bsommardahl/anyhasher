8594e9d1d6bc0a73e0842731ba4c2aaaf2cefeb0
Byron Sommardahl <byron@sommardahl.com>
2022-09-20 17:54:28 -0500

    Adding backend ts.

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -4,12 +4,13 @@ import cors from 'cors';
 import hashRoutes from './routes/hash.routes.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use('/hash', hashRoutes);
 
 app.listen(port, () => {
+    // tslint:disable-next-line
     console.log(`Host listening at http://localhost:${port}`);
 });
 
