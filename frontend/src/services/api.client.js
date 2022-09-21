a10d2b2254f2e7d868bee18b9bf381127b2e9101
Byron Sommardahl <byron@sommardahl.com>
2022-09-21 01:45:29 -0500

    Fixing eb url for frontend.

--- frontend/src/services/api.client.js
+++ frontend/src/services/api.client.js
@@ -1,6 +1,6 @@
 export async function getHashedValue(value) {
-    const apiHost = process.env.REACT_APP_API_HOST || 'localhost';
-    const response = await fetch(`http://${apiHost}:5000/hash/${value}`);
+    const apiHost = process.env.REACT_APP_API_HOST || 'localhost:5000';
+    const response = await fetch(`http://${apiHost}/hash/${value}`);
     const hash = await response.text();
     return hash;
 }
