0f81ecd1878ec716c6817ef2117b1bc0dc96afb6
Byron Sommardahl <byron@sommardahl.com>
2022-09-20 23:36:56 -0500

    Cleaning up stryker files

--- backend/.stryker-tmp/sandbox7004587/dist/routes/hash.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const hash_controller_js_1 = require("../controllers/hash.controller.js");
-const router = (0, express_1.Router)();
-router
-    .route('/:value')
-    .get(hash_controller_js_1.default.getHash);
-exports.default = router;
--- backend/.stryker-tmp/sandbox7983994/dist/routes/hash.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import hashController from '../controllers/hash.controller.js';
-const router = Router();
-router
-    .route('/:value')
-    .get(hashController.getHash);
-export default router;
-//# sourceMappingURL=hash.routes.js.map
