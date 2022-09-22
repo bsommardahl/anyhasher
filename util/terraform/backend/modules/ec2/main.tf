98af1ff3fdcf6402127af25b2e5ce8c9547d9828
Byron Sommardahl <byron@sommardahl.com>
2022-09-22 01:30:45 -0500

    restructuring cicd

--- util/terraform/backend/modules/ec2/main.tf
+++ util/terraform/backend/modules/ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "hashing_app_server" {
+resource "aws_instance" "anyhasher_server" {
   ami           = "ami-068663a3c619dd892"
   instance_type = "t2.micro"
   key_name = "pearson1"
@@ -9,7 +9,7 @@ resource "aws_instance" "hashing_app_server" {
   }
 }
 
-resource "aws_key_pair" "hashing_app_server_key" {
+resource "aws_key_pair" "anyhasher_server_key" {
   key_name   = "pearson1"
   public_key = var.public_key
 }
--- util/terraform/backend/modules/ec2/outputs.tf
+++ util/terraform/backend/modules/ec2/outputs.tf
@@ -1,3 +1,3 @@
 output "ec2_public_ip" {
-  value = aws_instance.hashing_app_server.public_ip
+  value = aws_instance.hanyhasher_server.public_ip
 }
