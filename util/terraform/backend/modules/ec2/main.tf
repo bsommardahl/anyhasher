cd33d1c9a6888837d95c43e71d58c4ff1a3a335e
Byron Sommardahl <byron@sommardahl.com>
2022-09-22 01:36:00 -0500

    restructuring cicd

--- util/terraform/backend/modules/ec2/main.tf
+++ util/terraform/backend/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "anyhasher_server" {
   ami           = "ami-068663a3c619dd892"
   instance_type = "t2.micro"
-  key_name = "pearson1"
+  key_name = "anyhasher"
 
   tags = {
     Name = var.instance_name,
@@ -10,6 +10,6 @@ resource "aws_instance" "anyhasher_server" {
 }
 
 resource "aws_key_pair" "anyhasher_server_key" {
-  key_name   = "pearson1"
+  key_name   = "anyhasher"
   public_key = var.public_key
 }
