0ff2a22e771b8470161b89c7bf617c023b5e48d9
Byron Sommardahl <byron@sommardahl.com>
2022-09-22 18:49:38 -0500

    Deploying and destroying.

--- util/terraform/backend/main.tf
+++ util/terraform/backend/main.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
+  
+  backend "s3" {
+    bucket = "anyhasher.terraform.states"
+    key    = "anyhasher.backend.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
