4b419ff47fa59dd5f4f948c1b214327039af7e4b
Byron Sommardahl <byron@sommardahl.com>
2022-09-22 01:43:54 -0500

    restructuring cicd

--- util/terraform/backend/modules/ec2/main.tf
+++ util/terraform/backend/modules/ec2/main.tf
@@ -1,15 +1,10 @@
 resource "aws_instance" "anyhasher_server" {
   ami           = "ami-068663a3c619dd892"
   instance_type = "t2.micro"
-  key_name = "anyhasher"
+  key_name = "pearson1"
 
   tags = {
     Name = var.instance_name,
     MadeWith = "Terraform"
   }
-}
-
-resource "aws_key_pair" "anyhasher_server_key" {
-  key_name   = "anyhasher"
-  public_key = var.public_key
 }
