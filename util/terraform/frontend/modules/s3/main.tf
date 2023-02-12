b7d59e547cb3388f34b57e46757669e028129406
Byron Sommardahl <byron@sommardahl.com>
2023-02-12 16:16:28 -0600

    Adding force destroy.

--- util/terraform/frontend/modules/s3/main.tf
+++ util/terraform/frontend/modules/s3/main.tf
@@ -15,6 +15,7 @@ data "aws_iam_policy_document" "website_policy" {
 
 resource "aws_s3_bucket" "frontend" {
   bucket = var.bucket_name  
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
