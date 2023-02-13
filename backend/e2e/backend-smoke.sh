0f5ef01bc06f61a91222904f8343999e21479711
Byron Sommardahl <byron@sommardahl.com>
2023-02-13 16:14:08 +0000

    Fixed typo in backend smoke test.

--- backend/e2e/backend-smoke.sh
+++ backend/e2e/backend-smoke.sh
@@ -1,4 +1,4 @@
-if curl -s ${BACKEND_URL}hash/test | grep "098f6bcd4621d373cade4e832627b4f6"
+if curl -s ${BACKEND_URL}/hash/test | grep "098f6bcd4621d373cade4e832627b4f6"
 then
     echo "Success!"
     exit 0
