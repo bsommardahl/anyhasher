22fafa24ec000a85597cba3ab8b00e988ca32100
Byron Sommardahl <byron@sommardahl.com>
2023-02-17 14:21:08 -0600

    Allowing http

--- util/terraform/router/modules/cloudfront/main.tf
+++ util/terraform/router/modules/cloudfront/main.tf
@@ -7,7 +7,7 @@ resource "aws_cloudfront_distribution" "production_distribution" {
     custom_origin_config {
       http_port = "80"
       https_port = "443"
-      #origin_protocol_policy = "http-only"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
@@ -17,6 +17,7 @@ resource "aws_cloudfront_distribution" "production_distribution" {
 
   default_cache_behavior {
     #viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
     compress = true
     allowed_methods = ["GET", "HEAD"]
     cached_methods = ["GET", "HEAD"]
