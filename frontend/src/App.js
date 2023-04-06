c61da28280f20dd52038715b06f6821b17c81a05
Byron Sommardahl <byron@sommardahl.com>
2023-04-06 09:23:31 -0500

    Resetting cicd jobs for OLT.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,48 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from 'react';
 import { Button, TextField, Paper } from '@material-ui/core';
-import * as apiClient from './services/api.client';
+import getHashedValue from './services/api.client';
 
 function App() {
-    const [valeToHash, setValueToHash] = useState('');
-    const [hashedValue, setHashedValue] = useState('');
+  const [valeToHash, setValueToHash] = useState('');
+  const [hashedValue, setHashedValue] = useState('');
 
-    function updateValueToHash(e) {
-        setValueToHash(e.target.value);
-    }
+  function updateValueToHash(e) {
+    setValueToHash(e.target.value);
+  }
 
-    async function getHashedValueFromApi() {
-        const hash = await apiClient.getHashedValue(valeToHash);
-        setHashedValue(hash);
-    }
+  async function getHashedValueFromApi() {
+    const hash = await getHashedValue(valeToHash);
+    setHashedValue(hash);
+  }
 
-    return (
-        <div className={'Div'}>
-            <h2 id="page-title">AnyHasher</h2>
-            <Paper>
-                <TextField
-                    fullWidth
-                    color='primary'
-                    size="medium"
-                    label={'Enter value to hash'}
-                    variant="outlined"
-                    value={valeToHash}
-                    onChange={updateValueToHash}/>
-            </Paper>
-            <br/>
-            <Button
-                variant="contained"
-                color="primary"
-                size='medium'
-                disabled={!valeToHash}
-                onClick={getHashedValueFromApi}>
-                GET HASH
-            </Button>
-            <p style={{fontWeight:'bolder'}}>{hashedValue}</p>
-        </div>
-    );
+  return (
+    <div className="Div">
+      <h2 id="page-title">AnyHasher</h2>
+      <Paper>
+        <TextField
+          fullWidth
+          color="primary"
+          size="medium"
+          label="Enter value to hash"
+          variant="outlined"
+          value={valeToHash}
+          onChange={updateValueToHash}
+        />
+      </Paper>
+      <br />
+      <Button
+        variant="contained"
+        color="primary"
+        size="medium"
+        disabled={!valeToHash}
+        onClick={getHashedValueFromApi}
+      >
+        GET HASH
+      </Button>
+      <p style={{ fontWeight: 'bolder' }}>{hashedValue}</p>
+    </div>
+  );
 }
 
 export default App;
