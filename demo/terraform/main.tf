138655bc319ca25267c1fc5fb29f6f906843bde6
Byron Sommardahl <byron@sommardahl.com>
2024-03-07 01:24:05 -0600

    Everything running well.

---
+++ demo/terraform/main.tf
@@ -0,0 +1,39 @@
+# terraform {
+  # required_providers {
+  #   aws = {
+  #     source = "hashicorp/aws"
+  #     version = "~> 4.16"
+  #   }
+  # }
+  # required_version = ">= 1.2.0"
+  
+  # backend "s3" {
+  #   bucket = "anyhasher.terraform.states"
+  #   key    = "anyhasher-be.tfstate"
+  #   region = "us-east-1"
+  # }
+# }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "anyhasher_server" {
+  ami           = "ami-0149b2da6ceec4bb0"
+  instance_type = "t2.micro"
+  key_name      = "anyhasher"
+  tags          = { Name = var.instance_name }
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.anyhasher_server.public_ip
+}
+
+output "ec2_public_url" {
+  value = aws_instance.anyhasher_server.public_dns
+}
+
