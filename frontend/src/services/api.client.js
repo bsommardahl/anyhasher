eecf6e27433d21f319d8abef74ebeca094247969
Byron Sommardahl <byron@sommardahl.com>
2024-10-23 14:14:45 -0500

    Harmonizing port numbers for backend server.

--- frontend/src/services/api.client.js
+++ frontend/src/services/api.client.js
@@ -1,5 +1,5 @@
 export default async function getHashedValue(value) {
-  const apiHost = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+  const apiHost = process.env.REACT_APP_API_URL || 'http://localhost:5001';
   const response = await fetch(`${apiHost}/hash/${value}`);
   const hash = await response.text();
   return hash;
