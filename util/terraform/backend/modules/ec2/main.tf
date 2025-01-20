c189e180be35a528b75400a4ac9cbcaca3925248
Byron Sommardahl <byron@sommardahl.com>
2025-01-20 15:29:06 -0600

    Disabling failure cleanup for troubleshooting.

--- util/terraform/backend/modules/ec2/main.tf
+++ util/terraform/backend/modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "anyhasher_server" {
-  ami           = "ami-0149b2da6ceec4bb0"
+  ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.small"
   key_name      = "anyhasher"
   tags          = { 
