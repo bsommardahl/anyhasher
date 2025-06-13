771294b1a8b28df1a6c222399ae1f5699c2aa974
Byron Sommardahl <byron@sommardahl.com>
2025-06-13 13:21:20 -0600

    Fixing tf s3 backend

--- util/terraform/versions.tf
+++ util/terraform/versions.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "anyhasher.terraform.states"
+    key    = "anyhasher-be.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
