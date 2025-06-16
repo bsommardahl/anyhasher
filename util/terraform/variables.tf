dabac84380bf016cdbec005174f6fa7ea593d9c0
Byron Sommardahl <byron@sommardahl.com>
2025-06-16 15:56:03 -0600

    Adding ver to root vars.

--- util/terraform/variables.tf
+++ util/terraform/variables.tf
@@ -8,3 +8,7 @@ variable "route53_record_name" {}
 variable "ami_id" {}
 variable "key_name" {}
 variable "instance_type" {}
+variable "ver" {
+  description = "Git version or tag for this deployment"
+  type        = string
+}
