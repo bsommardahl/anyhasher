56c94f39699a147acc112a55b50c6177fe1895b0
Byron Sommardahl <byron@sommardahl.com>
2025-06-16 16:10:07 -0600

    Improving cert.

--- util/terraform/modules/alb/cert.tf
+++ util/terraform/modules/alb/cert.tf
@@ -3,21 +3,14 @@ resource "aws_acm_certificate" "cert" {
   validation_method   = "DNS"
 }
 
-# Deduplicate by record name
-locals {
-  unique_validations = distinct([
-    for dvo in aws_acm_certificate.cert.domain_validation_options : {
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    dvo.resource_record_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
       value = dvo.resource_record_value
     }
-  ])
-}
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for record in local.unique_validations :
-    record.name => record
   }
 
   zone_id = var.route53_zone_id
@@ -29,5 +22,5 @@ resource "aws_route53_record" "cert_validation" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
 }
