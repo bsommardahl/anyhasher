817bc570c56cdd04fd8398a49b99f80a07b394e6
Byron Sommardahl <byron@sommardahl.com>
2025-06-16 16:13:01 -0600

    Fixing cert again

--- util/terraform/modules/alb/cert.tf
+++ util/terraform/modules/alb/cert.tf
@@ -1,11 +1,11 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name         = "*.${var.domain_root}"
-  validation_method   = "DNS"
+  domain_name       = "*.${var.domain_root}"
+  validation_method = "DNS"
 }
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    for dvo in tolist(aws_acm_certificate.cert.domain_validation_options) :
     dvo.resource_record_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
