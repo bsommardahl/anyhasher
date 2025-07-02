894c81567a5e69b458b25a37d15988181b6175e0
Felipe Perez <felipe.perez@varsity.dev>
2025-07-02 18:29:25 -0500

    Remove unused variable from frontend main

--- util/terraform/frontend/main.tf
+++ util/terraform/frontend/main.tf
@@ -1,7 +1,6 @@
 module "s3" {
-  source          = "./modules/s3"
-  environment     = var.environment
-  ver             = var.ver
-  route53_zone_id = var.route53_zone_id
+  source      = "./modules/s3"
+  environment = var.environment
+  ver         = var.ver
 }
 
