cee873f3448eb632cefb597e097ddc909ccb24bd
Felipe Perez <felipe.perez@varsity.dev>
2025-07-02 14:46:55 -0500

    Improve e2e smoke test and change gh action to execute puppeteer

--- frontend/e2e/smoke.e2e.ts
+++ frontend/e2e/smoke.e2e.ts
@@ -1,20 +1,20 @@
 import puppeteer from "puppeteer";
 
-async function getElement(page: puppeteer.Page, selector: string){
+async function getElement(page: puppeteer.Page, selector: string) {
   await page.waitForSelector(selector);
   const element = await page.$(selector);
   if (!element) throw new Error(`Element "${selector}" not found on page.`);
   return element;
 }
 
-async function getElementValue(page: puppeteer.Page, selector: string){
+async function getElementValue(page: puppeteer.Page, selector: string) {
   const element = await getElement(page, selector);
   const value = await element.evaluate((el: any) => el.textContent);
   if (!value) throw new Error(`Element "${selector}" had no value.`);
   return value;
 }
 
-async function verifyElementContainsText(page: puppeteer.Page, selector: string, expectedText: string){
+async function verifyElementContainsText(page: puppeteer.Page, selector: string, expectedText: string) {
   const value = await getElementValue(page, selector);
   console.log(`Found: ${value}`);
   const found = value.toLowerCase().includes(expectedText.toLowerCase());
@@ -23,13 +23,13 @@ async function verifyElementContainsText(page: puppeteer.Page, selector: string,
   }
 }
 
-async function enterText(page: puppeteer.Page, selector: string, text: string){
+async function enterText(page: puppeteer.Page, selector: string, text: string) {
   const element = await getElement(page, selector);
   await element.type(text);
 }
 
-async function clickElement(page: puppeteer.Page, selector: string){
-  const element = await getElement(page, selector);  
+async function clickElement(page: puppeteer.Page, selector: string) {
+  const element = await getElement(page, selector);
   await element.click();
 }
 
@@ -41,17 +41,18 @@ async function main(url: string) {
     await verifyElementContainsText(page, "#page-title", "anyhasher");
     await enterText(page, "input[type=text]", "This had BETTER work!");
     await clickElement(page, "button");
-    //await verifyElementContainsText(page, "p", "d6115deb306b9655598232c871ef6a04");
+    await page.waitForTimeout(2000);
+    await verifyElementContainsText(page, ".Div p", "d6115deb306b9655598232c871ef6a04");
     console.log("Smoke test passed.");
-    (<any>process).exitCode = 0;
+    (process as any).exitCode = 0;
   } catch (err) {
-    (<any>process).exitCode = 1;
+    (process as any).exitCode = 1;
     throw err;
   } finally {
-    await browser.close();    
+    await browser.close();
   }
 }
 
-const url = <any>process.env.TEST_URL || "http://localhost:3000/";
-if (!url) throw Error("No TEST_URL defined.");    
+const url = process.env.TEST_URL as any || "http://localhost:3000/";
+if (!url) throw Error("No TEST_URL defined.");
 main(url);
