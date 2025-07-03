39b7fecffc3f820678ee304ebd0af7b84ec58c08
Felipe Perez <felipe.perez@varsity.dev>
2025-07-02 19:21:18 -0500

    Modify smoke e2e tests execution on pipeline

--- util/terraform/frontend/modules/s3/s3.tf
+++ util/terraform/frontend/modules/s3/s3.tf
@@ -67,7 +67,7 @@ resource "aws_route53_record" "frontend" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.frontend.website_domain
+    name                   = aws_s3_bucket_website_configuration.frontend.website_endpoint
     zone_id                = aws_s3_bucket.frontend.hosted_zone_id
     evaluate_target_health = false
   }
