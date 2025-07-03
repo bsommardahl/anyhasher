5dd94355889348814ad6371c4434b5fe41f0792b
Felipe Perez <felipe.perez@varsity.dev>
2025-07-02 22:50:26 -0500

    Clean up

--- frontend/src/services/api.client.js
+++ frontend/src/services/api.client.js
@@ -1,6 +1,6 @@
 export async function getHashedValue(value) {
-    const apiHost = 'http://localhost:5001';
-    const response = await fetch(`${apiHost}/hash/${value}`);
-    const hash = await response.text();
-    return hash;
+  const apiHost = 'http://localhost:5001';
+  const response = await fetch(`${apiHost}/hash/${value}`);
+  const hash = await response.text();
+  return hash;
 }
