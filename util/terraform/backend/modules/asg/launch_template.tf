cc4cd57c817fd20dab4318dfd5760b92269d5f78
Byron Sommardahl <byron@sommardahl.com>
2025-08-25 15:25:03 -0500

    Improving instance names.

--- util/terraform/backend/modules/asg/launch_template.tf
+++ util/terraform/backend/modules/asg/launch_template.tf
@@ -21,8 +21,7 @@ resource "aws_launch_template" "this" {
   tag_specifications {
     resource_type = "instance"
     tags = {
-      //Name           = "anyhasher-${var.environment}-${substr(var.ver, -6, -1)}"
-      Name           = "anyhasher-${substr(var.ver, -6, -1)}"
+      Name           = "anyhasher-${var.environment}-${var.deployment_type}-${substr(var.ver, -6, -1)}"
       Version        = var.ver
       Deployment     = "${var.environment}-${var.deployment_type}-${var.ver}"
       DeploymentType = var.deployment_type
