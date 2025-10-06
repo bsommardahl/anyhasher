1afce3478a1becfbe1ea81ad329e049235cc51bc
Byron Sommardahl <byron@sommardahl.com>
2025-10-06 12:39:28 -0500

    Moving role to proper folder. Adding e2e test for BE.

--- backend/e2e/backend-smoke.sh
+++ backend/e2e/backend-smoke.sh
@@ -6,3 +6,11 @@ else
     echo "Correct hash not found!"
     exit 1
 fi
+
+if curl -s ${BACKEND_URL}/feature-flags/enhanced-response | grep '"enabled": *true'
+then
+    echo "Enhanced response feature flag is enabled!"
+else
+    echo "Enhanced response feature flag is not enabled!"
+    exit 1
+fi
