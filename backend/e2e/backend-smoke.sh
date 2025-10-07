28cf35086ee1e8ef8b3c55742548e3a8b4221d96
Byron Sommardahl <byron@sommardahl.com>
2025-10-07 09:43:09 -0500

    Enhancing e2e test to handle changing feature flags.

--- backend/e2e/backend-smoke.sh
+++ backend/e2e/backend-smoke.sh
@@ -6,11 +6,12 @@ else
     exit 1
 fi
 
-if curl -s ${BACKEND_URL}/feature-flags/enhanced_response | grep '"enabled": *true'
-then
-    echo "Enhanced response feature flag is enabled!"
+ENHANCED_RESPONSE_ENABLED=$(grep "^FEATURE_ENHANCED_RESPONSE=" /.feature-flags | cut -d'=' -f2)
+if curl -s ${BACKEND_URL}/feature-flags/enhanced_response | grep "\"enabled\": *${ENHANCED_RESPONSE_ENABLED}"
+then}
+    echo "Enhanced response feature flag is ${ENHANCED_RESPONSE_ENABLED} (correct)."
 else
-    echo "Enhanced response feature flag is not enabled!"
+    echo "Enhanced response feature flag is not ${ENHANCED_RESPONSE_ENABLED} (incorrect)!"
     exit 1
 fi
 
